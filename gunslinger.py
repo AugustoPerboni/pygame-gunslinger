4c406e081f304be74771cb6101ce680d5d52a97a
Augusto Perboni <120643495+AugustoPerboni@users.noreply.github.com>
2023-02-06 02:04:25 +0000

    Collisions character/character and bullet/enemy
    The hitbox was defined for all elements. If some collision occurs it will be printed in the cmd.

--- gunslinger.py
+++ gunslinger.py
@@ -18,47 +18,7 @@ class element:
         self.width = width
         self.height = height
         self.velocity = velocity
-
-class Enemy(element):
-    image_walk_right = [pygame.image.load('R1E.png'), pygame.image.load('R2E.png'), pygame.image.load('R3E.png'), pygame.image.load('R4E.png'), pygame.image.load('R5E.png'), pygame.image.load('R6E.png'), pygame.image.load('R7E.png'), pygame.image.load('R8E.png'), pygame.image.load('R9E.png'), pygame.image.load('R10E.png'), pygame.image.load('R11E.png')]
-    image_walk_left = [pygame.image.load('L1E.png'), pygame.image.load('L2E.png'), pygame.image.load('L3E.png'), pygame.image.load('L4E.png'), pygame.image.load('L5E.png'), pygame.image.load('L6E.png'), pygame.image.load('L7E.png'), pygame.image.load('L8E.png'), pygame.image.load('L9E.png'), pygame.image.load('L10E.png'), pygame.image.load('L11E.png')]
-
-    def __init__(self,x,y):
-        element.__init__(self,x,y,40,60,3)
-        self.walk_count = 0
-        self.walk_right = False
-        self.walk_left = True
-
-    def draw(self,window):
-        self.move()
-        # Each image will be used in 3 frames
-        if self.walk_count +1 >= 33:
-            self.walk_count = 0
-        
-        # test if i need the self in image
-        if self.walk_left:
-            window.blit(self.image_walk_left[self.walk_count//3], (self.x,self.y))
-            self.walk_count += 1
-        elif self.walk_right:
-            window.blit(self.image_walk_right[self.walk_count//3], (self.x,self.y))
-            self.walk_count += 1 
-
-    def move(self):
-        if self.x <= self.width:
-            self.walk_left = False
-            self.walk_right = True
-            self.walk_count = 0
-        elif self.x >= window_width - self.width:
-            self.walk_left = True
-            self.walk_right = False
-            self.walk_count = 0
-
-        if self.walk_left:
-            self.x -= self.velocity
-        elif self.walk_right:
-            self.x += self.velocity
-
-
+        self.hitbox = ()
 
 class Player(element):
     image_walk_right = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), 
@@ -74,8 +34,9 @@ class Player(element):
     pygame.image.load('L9.png')]
 
     def __init__(self):
-        element.__init__(self,50,400,40,60,5)
+        element.__init__(self,50,400,64,64,5)
 
+        self.hitbox = (self.x + 17, self.y + 11, 25, 52 )
         self.jump_count = 0
         self.is_jump = False 
         
@@ -101,6 +62,55 @@ class Player(element):
                 window.blit(self.image_walk_left[self.walk_count//3], (self.x, self.y))
             else:
                 window.blit(self.image_walk_right[self.walk_count//3], (self.x, self.y))
+        # pygame.draw.rect(window,(255,0,0), self.hitbox,2)
+
+    def hit(self):
+        print('PLAYER HIT')
+class Enemy(element):
+    image_walk_right = [pygame.image.load('R1E.png'), pygame.image.load('R2E.png'), pygame.image.load('R3E.png'), pygame.image.load('R4E.png'), pygame.image.load('R5E.png'), pygame.image.load('R6E.png'), pygame.image.load('R7E.png'), pygame.image.load('R8E.png'), pygame.image.load('R9E.png'), pygame.image.load('R10E.png'), pygame.image.load('R11E.png')]
+    image_walk_left = [pygame.image.load('L1E.png'), pygame.image.load('L2E.png'), pygame.image.load('L3E.png'), pygame.image.load('L4E.png'), pygame.image.load('L5E.png'), pygame.image.load('L6E.png'), pygame.image.load('L7E.png'), pygame.image.load('L8E.png'), pygame.image.load('L9E.png'), pygame.image.load('L10E.png'), pygame.image.load('L11E.png')]
+
+    def __init__(self,x,y):
+        element.__init__(self,x,y,64,64,3)
+
+        self.hitbox = (self.x + 17, self.y +2, 31, 57 )
+        self.walk_count = 0
+        self.walk_right = False
+        self.walk_left = True
+
+    def draw(self,window):
+        self.move()
+        # Each image will be used in 3 frames
+        if self.walk_count +1 >= 33:
+            self.walk_count = 0
+        
+        # test if i need the self in image
+        if self.walk_left:
+            window.blit(self.image_walk_left[self.walk_count//3], (self.x,self.y))
+            self.walk_count += 1
+        elif self.walk_right:
+            window.blit(self.image_walk_right[self.walk_count//3], (self.x,self.y))
+            self.walk_count += 1 
+        # pygame.draw.rect(window,(255,0,0), self.hitbox,2)
+    def hit(self):
+        print('ENEMY HIT')
+
+    def move(self):
+        if self.x <= 0:
+            self.walk_left = False
+            self.walk_right = True
+            self.walk_count = 0
+        elif self.x >= window_width - self.width:
+            self.walk_left = True
+            self.walk_right = False
+            self.walk_count = 0
+
+        if self.walk_left:
+            self.x -= self.velocity
+        elif self.walk_right:
+            self.x += self.velocity
+        self.hitbox = (self.x + 17, self.y +2, 31, 57 )
+
             
 class projectile(element):
 
@@ -108,18 +118,18 @@ class projectile(element):
         element.__init__(self,x,y,10,3,15)
         # 1 = right, -1 = left
         self.facing = facing
-    def draw(self,window):
-        
+
+    def draw(self,window):  
         pygame.draw.rect(window, (0,0,0), (self.x,self.y,self.width,self.height))
 
-def redraw_game_window(player,bullets,enemy,window): 
+def redraw_game_window(player,bullets,enemies,window): 
     # Add the background
     window.blit(background, (0,0))
     # Add the player
     player.draw(window)
-    for bullet in bullets:
-        bullet.draw(window)
-    enemy.draw(window)
+
+    [bullet.draw(window) for bullet in bullets]
+    [enemy.draw(window) for enemy in enemies]
     pygame.display.update()
 
 # Game main loop
@@ -127,7 +137,10 @@ def redraw_game_window(player,bullets,enemy,window):
 player = Player()
 bullets=[]
 prog_run = True
-enemy = Enemy(300,400) # Enemy initial position
+enemies = []
+enemies.append(Enemy(300,400)) # Enemy initial position
+enemies.append(Enemy(100,400))
+
 
 while prog_run:
     clock.tick(27) # Make the fresh rate lower than 27 fps 
@@ -136,12 +149,26 @@ while prog_run:
         if event.type == pygame.QUIT: 
             prog_run = False
     
-    
+    # Bullet collision
     for bullet in bullets:
+        for i in range(len(enemies)):
+            if bullet.y > enemies[i].y and bullet.y < enemies[i].y + enemies[i].height and bullet.x > enemies[i].x and bullet.x < enemies[i].x + enemies[i].width:
+                enemies[i].hit()
+                if bullet in bullets: # If used to prevent error in the bullet removal    
+                    bullets.pop(bullets.index(bullet))
         if bullet.x < 0 or bullet.x > 500:
             bullets.pop(bullets.index(bullet))
         else:
             bullet.x = bullet.x + bullet.facing * bullet.velocity
+
+    # Characters collision
+    for enemy in enemies:
+        if (player.hitbox[1] + player.hitbox[2]) > enemy.hitbox[1]:
+            
+            if enemy.hitbox[0]< player.hitbox[0] < (enemy.hitbox[0] + enemy.hitbox[2]):
+                player.hit()
+            elif enemy.hitbox[0] < (player.hitbox[0] + player.hitbox[2]) < (enemy.hitbox[0] + enemy.hitbox[2]):
+                player.hit()
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_SPACE]:
@@ -186,8 +213,8 @@ while prog_run:
             player.is_jump = False
     
     
-
-    redraw_game_window(player,bullets,enemy,game_window)
+    player.hitbox = (player.x + 17, player.y + 11, 25, 52 )
+    redraw_game_window(player,bullets,enemies,game_window)
                 
 
 
