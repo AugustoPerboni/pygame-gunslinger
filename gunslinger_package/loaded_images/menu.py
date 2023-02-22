7d1a528d1162849a6768e12ab346607ee6c22d6c
AugustoPerboni <augustoperboni@tecnico.ulisboa.pt>
2023-02-22 01:04:45 +0000

    Executable file
    Now the game can be played by executing  "gunslinger_play" file

--- gunslinger_package/loaded_images/menu.py
+++
@@ -1,3 +0,0 @@
-import pygame
-coin_image = pygame.image.load('images\menu\\coin.png')
-barra_coin = pygame.image.load('images\menu\\ButonsMG.png')
--- gunslinger_package/menu_elements/button.py
+++
@@ -1,6 +0,0 @@
-class Button:
-    def __init__(self,x,y,hitbox_width,hitbox_height,image):
-        self.x = x
-        self.y = y
-        hitbox = (x,y,hitbox_width,hitbox_height)
-        self.image = image
